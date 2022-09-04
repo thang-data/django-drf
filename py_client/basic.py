848d1b6499bb719c361b0e09868668c1d87c44f8
thang-data <nguyenducthang.iy1908@gmail.com>
2022-09-04 11:04:30 +0700

    api

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
-endpoint ='https://httpbin.org/status/200'
-endpoint = 'https://httpbin.org/anything'
+# endpoint ='https://httpbin.org/status/200'
+endpoint = 'http://localhost:1323/api'
 
 get_response = requests.get(endpoint, json={
     "query": "Hello world"
 })
 
 # print(get_response.text)
-print(get_response.json())
+# print(get_response.json())
+print(get_response.status_code)
